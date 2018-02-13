0b078a298232f54b63b4b504b42058da1560b022
ITry321 <iratytar1217@gmail.com>
2018-02-13 21:58:12 +0200

    new'

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("First hello");
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function () {
+    var vm = this;
+
+    vm.class = [
+        {
+            header: "header1",
+            aside: "aside1",
+            main: "main1"
+        },
+        {
+            header: "header2",
+            aside: "aside2",
+            main: "main2"
+        },
+        {
+            header: "header3",
+            aside: "aside3",
+            main: "main3"
+        },
+    ];
+    vm.style1 = function(){
+       vm.newStyle = vm.class[0]; 
+        console.log(vm.newStyle);
+    };
+   vm.style2 = function(){
+       vm.newStyle = vm.class[1]; 
+        console.log(vm.newStyle);
+    }; 
+    vm.style3 = function(){
+       vm.newStyle = vm.class[2]; 
+        console.log(vm.newStyle);
+    }; 
+    
+});
